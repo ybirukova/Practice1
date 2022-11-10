3ea0588f53878a1077b8741efc5b3dea6b1cce64
yanabirukova <y.birukova@gmail.com>
2022-11-10 23:16:04 +0300

    task2 is done

--- app/src/main/java/com/example/practice1/Homework.java
+++ app/src/main/java/com/example/practice1/Homework.java
@@ -1,6 +1,30 @@
 package com.example.practice1;
 
+import java.util.Scanner;
+
 public class Homework {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+        defineTheSmallestNumber();
     }
+
+    static void defineTheSmallestNumber() {
+        System.out.println("Введите три числа.");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        a = (a < 0) ? (-a) : a;
+        b = (b < 0) ? (-b) : b;
+        c = (c < 0) ? (-c) : c;
+
+        if (a <= b && a <= c) {
+            System.out.println("Наименьшее по модулю число: " + a);
+        } else if (b <= a && b <= c) {
+            System.out.println("Наименьшее по модулю число: " + b);
+        } else if (c <= a && c <= b) {
+            System.out.println("Наименьшее по модулю число: " + b);
+        }
+    }
+
 }
