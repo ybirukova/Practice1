c43916b47bf326a12cdbda4960069dd1a3463c1d
yanabirukova <y.birukova@gmail.com>
2022-11-11 16:13:12 +0300

    task3 is done

--- app/src/main/java/com/example/practice1/Homework.java
+++ app/src/main/java/com/example/practice1/Homework.java
@@ -1,6 +1,40 @@
 package com.example.practice1;
 
+import java.util.Scanner;
+
 public class Homework {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+        printDayOfTheWeek();
+    }
+
+    static void printDayOfTheWeek() {
+        System.out.println("Введите любое число в промежутке от 1 до 7");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        switch (a) {
+            case 1:
+                System.out.println("Понедельник");
+                break;
+            case 2:
+                System.out.println("Вторник");
+                break;
+            case 3:
+                System.out.println("Среда");
+                break;
+            case 4:
+                System.out.println("Четверг");
+                break;
+            case 5:
+                System.out.println("Пятница");
+                break;
+            case 6:
+                System.out.println("Суббота");
+                break;
+            case 7:
+                System.out.println("Воскресенье");
+                break;
+            default:
+                System.out.println("Неизвестный день недели");
+        }
     }
 }
