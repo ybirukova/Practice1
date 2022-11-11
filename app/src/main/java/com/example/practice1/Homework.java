73cc423294edebd1610a381b235deb8072192a80
yanabirukova <y.birukova@gmail.com>
2022-11-11 17:22:04 +0300

    task4 is done

--- app/src/main/java/com/example/practice1/Homework.java
+++ app/src/main/java/com/example/practice1/Homework.java
@@ -1,6 +1,14 @@
 package com.example.practice1;
 
 public class Homework {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+        everyFifthNumber();
+    }
+
+    static void everyFifthNumber() {
+        for (int i = 0; i <= 100; i += 5) {
+            if (i == 0) continue;
+            System.out.print(i + " ");
+        }
     }
 }
