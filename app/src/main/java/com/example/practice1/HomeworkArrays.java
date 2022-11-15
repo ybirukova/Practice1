d0d8a02538e72da21846c9dbb2a75926ee17f695
yanabirukova <y.birukova@gmail.com>
2022-11-15 17:19:35 +0300

    arrayTask2 is done

--- app/src/main/java/com/example/practice1/HomeworkArrays.java
+++ app/src/main/java/com/example/practice1/HomeworkArrays.java
@@ -1,8 +1,49 @@
 package com.example.practice1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class HomeworkArrays {
     public static void main(String[] args) {
-
+        remakeRandomArrayToEvenArray();
     }
 
+    static void remakeRandomArrayToEvenArray() {
+        System.out.println("Чтобы задать размер массива, введите любое число больше 5 и меньше либо равно 10.");
+        Scanner in = new Scanner(System.in);
+        int arraySize = in.nextInt();
+
+        while (arraySize <= 5 || arraySize > 10) {
+            System.out.println("Чтобы задать размер массива, введите любое число больше 5 и меньше либо равно 10.");
+            arraySize = in.nextInt();
+        }
+
+        int[] arrayOfRandomNumbers = new int[arraySize];
+
+        for (int i = 0; i < arrayOfRandomNumbers.length; i++) {
+            arrayOfRandomNumbers[i] = (int) (Math.random() * 100);
+        }
+
+        System.out.println("Сгенерированный массив: " + Arrays.toString(arrayOfRandomNumbers));
+
+        int counterOfEven = 0;
+        for (int var : arrayOfRandomNumbers) {
+            counterOfEven = var % 2 == 0 ? ++counterOfEven : counterOfEven;
+        }
+
+        int[] arrayOfEvenNumbers = new int[counterOfEven];
+
+        if (counterOfEven == 0) {
+            System.out.println("Четных чисел в массиве нет.");
+        } else {
+            int i = 0;
+            for (int var : arrayOfRandomNumbers) {
+                if (var % 2 == 0) {
+                    arrayOfEvenNumbers[i] = var;
+                    i++;
+                }
+            }
+            System.out.println("Массив из четных чисел первоначального массива: " + Arrays.toString(arrayOfEvenNumbers));
+        }
+    }
 }
