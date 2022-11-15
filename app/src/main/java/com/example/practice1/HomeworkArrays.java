162176e232065887c7a5a6c0fac43a8e7ab9e1b0
yanabirukova <y.birukova@gmail.com>
2022-11-15 16:12:35 +0300

    arrayTask1 is done

--- app/src/main/java/com/example/practice1/HomeworkArrays.java
+++ app/src/main/java/com/example/practice1/HomeworkArrays.java
@@ -1,8 +1,15 @@
 package com.example.practice1;
 
+import java.util.Arrays;
+
 public class HomeworkArrays {
     public static void main(String[] args) {
-
+        createArrayOfNames();
     }
 
+    static void createArrayOfNames (){
+        String[] arrayOfNames = new String[]{"Yana", "Max", "Maria", "Helga", "Anna", "Timur", "Egor"};
+        Arrays.sort(arrayOfNames);
+        System.out.println(Arrays.toString(arrayOfNames));
+    }
 }
