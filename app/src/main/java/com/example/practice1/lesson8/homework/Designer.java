b103199a01c2b247e6982d645d2764ddb3df3165
yanabirukova <y.birukova@gmail.com>
2022-11-29 01:42:20 +0300

    change good work good

--- app/src/main/java/com/example/practice1/lesson8/homework/Designer.java
+++ app/src/main/java/com/example/practice1/lesson8/homework/Designer.java
@@ -1,6 +1,6 @@
 package com.example.practice1.lesson8.homework;
 
-public class Designer extends Employee{
+public class Designer extends Employee {
     @Override
     public void printInfo() {
         System.out.println("Designer has: ");
--- app/src/main/java/com/example/practice1/lesson8/homework/Employee.java
+++ app/src/main/java/com/example/practice1/lesson8/homework/Employee.java
@@ -1,9 +1,9 @@
 package com.example.practice1.lesson8.homework;
 
 abstract public class Employee {
-    Workplace workplace = new Workplace("HP", "Logitech", "Logitech", "Samsung");
+    Workplace workplace = new Workplace("Macbook");
 
     public void printInfo() {
-        System.out.println("system unit: " + workplace.systemUnit + " keyword: " + workplace.keyword + " mouse: " + workplace.mouse + " monitor: " + workplace.monitor);
+        System.out.println("laptop: " + workplace.laptop);
     }
 }
