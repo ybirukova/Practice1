3cf793e8ed523c74f93d415d9571f87972b81e95
yanabirukova <y.birukova@gmail.com>
2022-12-06 20:59:55 +0300

    mistakes are corrected

--- app/src/main/java/com/example/practice1/lesson10/homework/App.java
+++ app/src/main/java/com/example/practice1/lesson10/homework/App.java
@@ -1,12 +1,8 @@
 package com.example.practice1.lesson10.homework;
 
 public class App {
-    public static void main(String[] args) throws WrongDocNumberException {
-        String docNumber = Document.createNumberOfDoc();
-        Document.getFirst8Numbers(docNumber);
-        Document.changeLetters(docNumber);
-        Document.printLetters(docNumber);
-        Document.checkContaining(docNumber, "aBc");
-        Document.checkEndOfString(docNumber);
+    public static void main(String[] args){
+        String docNumber = Document.enterNumberOfDoc();
+        Document.printInfoDocNumber(docNumber);
     }
 }
--- app/src/main/java/com/example/practice1/lesson10/homework/Document.java
+++ app/src/main/java/com/example/practice1/lesson10/homework/Document.java
@@ -6,8 +6,14 @@ import java.util.regex.Pattern;
 public class Document {
     public static final String PATTERN_LETTERS = "[A-Za-z]{1,10}";
     public static final String PATTERN_NUMBERS = "[\\d]{1,10}";
+    public static final int LETTERS_LENGTH = 8;
+    public static final int NUMBERS_LENGTH = 10;
+    public static final int HYPHENS_LENGTH = 4;
+    public static final int[] L_POS = {5, 8, 14, 17, 19, 21};
+    public static final int[] N_POS = {0, 4, 9, 13, 18, 20};
+    public static final int[] H_POS = {4, 8, 13, 17};
 
-    public static String createNumberOfDoc() throws WrongDocNumberException {
+    public static String enterNumberOfDoc() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите номер документа в формате xxxx-yyy-xxxx-yyy-xyxy, где x — число, а y — латинская буква.");
         String docNumber = scanner.nextLine();
@@ -18,9 +24,8 @@ public class Document {
             }
         } catch (StringIndexOutOfBoundsException | WrongDocNumberException indexOut) {
             System.out.println(indexOut);
-            createNumberOfDoc();
+            enterNumberOfDoc();
         }
-
         return docNumber;
     }
 
@@ -28,23 +33,25 @@ public class Document {
         StringBuilder letters = new StringBuilder();
         StringBuilder numbers = new StringBuilder();
         StringBuilder hyphens = new StringBuilder();
-        char[] charArray = docNumber.toCharArray();
 
-        for (char docChar : charArray) {
-            String charToString = String.valueOf(docChar);
+        for (int i = 0; i < docNumber.length(); i++) {
+            String charToString = String.valueOf(docNumber.charAt(i));
             if (Pattern.matches(PATTERN_LETTERS, charToString)) {
-                letters.append(docChar);
+                letters.append(docNumber.charAt(i));
             } else if (Pattern.matches(PATTERN_NUMBERS, charToString)) {
-                numbers.append(docChar);
+                numbers.append(docNumber.charAt(i));
             } else if (Pattern.matches("-", charToString)) {
-                hyphens.append(docChar);
+                hyphens.append(docNumber.charAt(i));
             }
         }
-        boolean isLengthCorrect = ((letters.length() == 8) && (numbers.length() == 10) && (hyphens.length() == 4));
 
-        String lettersOnPos = docNumber.substring(5, 8) + docNumber.substring(14, 17) + docNumber.charAt(19) + docNumber.charAt(21);
-        String numbersOnPos = docNumber.substring(0, 4) + docNumber.substring(9, 13) + docNumber.charAt(18) + docNumber.charAt(20);
-        String hyphensOnPos = ("" + docNumber.charAt(4) + docNumber.charAt(8) + docNumber.charAt(13) + docNumber.charAt(17));
+        boolean isLengthCorrect = ((letters.length() == LETTERS_LENGTH)
+                && (numbers.length() == NUMBERS_LENGTH)
+                && (hyphens.length() == HYPHENS_LENGTH));
+
+        String lettersOnPos = docNumber.substring(L_POS[0], L_POS[1]) + docNumber.substring(L_POS[2], L_POS[3]) + docNumber.charAt(L_POS[4]) + docNumber.charAt(L_POS[5]);
+        String numbersOnPos = docNumber.substring(N_POS[0], N_POS[1]) + docNumber.substring(N_POS[2], N_POS[3]) + docNumber.charAt(N_POS[4]) + docNumber.charAt(N_POS[5]);
+        String hyphensOnPos = ("" + docNumber.charAt(H_POS[0]) + docNumber.charAt(H_POS[1]) + docNumber.charAt(H_POS[2]) + docNumber.charAt(H_POS[3]));
         boolean isLettersPosCorrect = Pattern.matches(PATTERN_LETTERS, lettersOnPos);
         boolean isNumbersPosCorrect = Pattern.matches(PATTERN_NUMBERS, numbersOnPos);
         boolean isHyphensPosCorrect = Pattern.matches("----", hyphensOnPos);
@@ -53,7 +60,6 @@ public class Document {
         } else if (!isLengthCorrect) {
             throw new WrongDocNumberException("Номер документа не соответсвует формату.");
         }
-
         return true;
     }
 
@@ -62,7 +68,7 @@ public class Document {
         System.out.println("Два первых блока по 4 цифры: " + first8Numbers);
     }
 
-    public static void changeLetters(String docNumber) {
+    public static void hideLetters(String docNumber) {
         String newDocNumber = docNumber.substring(0, 5) + "***" + docNumber.substring(8, 14) + "***" + docNumber.substring(17);
         System.out.println("Номер документа (блоки из трех букв -> ***): " + newDocNumber);
     }
@@ -76,14 +82,11 @@ public class Document {
         str.insert(3, "/");
         str.insert(7, "/");
         str.insert(9, "/");
-
         System.out.println("Letters: " + str);
     }
 
     public static void checkContaining(String docNumber, String substring) {
-        String docNumberLC = docNumber.toLowerCase();
-        String substringLC = substring.toLowerCase();
-        if (docNumberLC.contains(substringLC)) {
+        if (docNumber.matches("(?i).*" + substring + ".*")) {
             System.out.println(docNumber + " содержит в себе " + substring + " (независимо от регистра).");
         } else
             System.out.println(docNumber + " не содержит в себе " + substring + " (независимо от регистра).");
@@ -96,5 +99,13 @@ public class Document {
             System.out.println(docNumber + " заканчивается на " + endStr + " (независимо от регистра).");
         } else
             System.out.println(docNumber + " не заканчивается на " + endStr + " (независимо от регистра).");
+    }
+
+    public static void printInfoDocNumber(String docNumber) {
+        getFirst8Numbers(docNumber);
+        hideLetters(docNumber);
+        printLetters(docNumber);
+        checkContaining(docNumber, "aBc");
+        checkEndOfString(docNumber);
     }
 }
