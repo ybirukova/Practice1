6a5e3e74cc461f7881b4cc91247272d587e5bc61
yanabirukova <y.birukova@gmail.com>
2022-12-12 19:45:24 +0300

    correct 2

--- app/src/main/java/com/example/practice1/lesson11/homework/User.java
+++ app/src/main/java/com/example/practice1/lesson11/homework/User.java
@@ -5,11 +5,10 @@ import java.util.Queue;
 
 public class User {
     private String userName;
-    private Queue<String> messages;
+    private Queue<String> messages= new LinkedList<>();
 
     User(String userName) {
         this.userName = userName;
-        this.messages = new LinkedList<>();
     }
 
     String getUserName() {
@@ -20,22 +19,13 @@ public class User {
         return messages;
     }
 
-    void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    void setMessages(Queue<String> messages) {
-        this.messages = messages;
-    }
-
     void writeToUser(String textOfMessage) {
         messages.add(textOfMessage);
     }
 
     void readAndDeleteMessage() {
         if (messages.peek() != null) {
-            System.out.println("Входящее сообщение: " + messages.peek());
-            messages.poll();
+            System.out.println("Входящее сообщение: " + messages.poll());
             System.out.println("Сообщение прочитано и удалено.");
         } else System.out.println("У пользователя нет новых сообщений");
     }
